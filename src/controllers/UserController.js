2a3f865e0a5a9a81a80676715864d6f1c86ffc60
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-09-24 22:34:51 -0300

    send email

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const Hackathon = require('../models/Hackathon');
+const mailer = require('../modules/mailer');
 
 module.exports = {
 	async store(req, res) {
@@ -12,6 +14,51 @@ module.exports = {
 			bio,
 			hackathon: hackaId
 		});
+
+		const hacka = await Hackathon.findOne({ _id: hackaId });
+
+		if (isMentor && role === 'Owner') {
+			mailer.sendMail(
+				{
+					to: email,
+					subject: 'Your hackathon its on',
+					template: '/hackathon_create',
+					context: {
+						hackatrack: `https://hacktrack-open-monitor.herokuapp.com/monitor/${hackaId}`,
+						mentor: hacka.mentorLink,
+						participant: hacka.participantLink
+					}
+				},
+				err => {
+					if (err) console.log(err);
+				}
+			);
+		} else if (isMentor) {
+			mailer.sendMail(
+				{
+					to: email,
+					subject: 'Your hackathon its on',
+					template: '/hackathon_mentor',
+					context: {
+						hackatrack: `https://hacktrack-open-monitor.herokuapp.com/monitor/${hackaId}`
+					}
+				},
+				err => {
+					if (err) console.log(err);
+				}
+			);
+		} else {
+			mailer.sendMail(
+				{
+					to: email,
+					subject: 'Your hackathon its on',
+					template: '/hackathon_participant'
+				},
+				err => {
+					if (err) console.log(err);
+				}
+			);
+		}
 
 		return res.json(user);
 	}
